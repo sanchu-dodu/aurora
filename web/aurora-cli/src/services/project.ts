0c9df45c10a9cf9bc51ba147663d661060b48ff5
sanchu-dodu <kingrussel499@gmail.com>
2026-07-23 16:03:50 +0300

    Milestone 7: Generate package.json

--- web/aurora-cli/src/services/project.ts
+++ web/aurora-cli/src/services/project.ts
@@ -2,6 +2,8 @@ import fs from "fs-extra";
 import path from "path";
 
 import { createProjectStructure } from "./filesystem.js";
+import { createPackageJson } from "./package.js";
+
 import type { ProjectConfig } from "../types/project.js";
 
 export async function createProject(
@@ -14,6 +16,9 @@ export async function createProject(
 
   // Create the folder structure
   await createProjectStructure(projectPath);
+
+  // Create package.json
+  await createPackageJson(projectPath, config);
 
   // Create aurora.config.json
   await fs.writeJson(
